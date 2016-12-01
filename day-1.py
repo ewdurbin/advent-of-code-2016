23ee9a1e30df6afb9dc65562c4979866edffe3b3
Ernest W. Durbin III <ewdurbin@gmail.com>
2016-12-01 17:12:49 -0500

    day one part two

--- day-1.py
+++ day-1.py
@@ -11,15 +11,22 @@ DIRECTIONS = {'N': {'R': 'E', 'L': 'W'},
 
 def main():
     input_data = sys.stdin.read().rstrip().split(', ')
-    totals = {'N': 0, 'E': 0, 'W': 0, 'S': 0}
+    location = {'N': 0, 'E': 0, 'W': 0, 'S': 0}
     orientation = 'N'
+    history = []
     for movement in input_data:
         direction = movement[0]
         distance = int(movement[1:])
-        totals[DIRECTIONS[orientation][direction]] += distance
+        for block in range(1,distance+1):
+            location[DIRECTIONS[orientation][direction]] += 1
+            coordinate = (location['N'] - location['S'], location['E'] - location['W'])
+            history.append(coordinate)
         orientation = DIRECTIONS[orientation][direction]
-    distance = abs(totals['N'] - totals['S']) + abs(totals['E'] - totals['W'])
-    print distance
+    for i, coordinate in enumerate(history):
+        if coordinate in history[:i]:
+            break
+    distance = abs(coordinate[0]) + abs(coordinate[1])
+    print(distance)
 
 if __name__ == '__main__':
     main()
