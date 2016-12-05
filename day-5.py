2f46e32f2716eee2c121d94f40b41ebe16a414c4
Ernest W. Durbin III <ewdurbin@gmail.com>
2016-12-05 00:30:13 -0500

    day five part two

--- day-5.py
+++ day-5.py
@@ -5,18 +5,20 @@ import hashlib
 def main():
     input_data = 'wtnhxymk'
     i = 0
-    code = ''
+    code = {'0': None, '1': None, '2': None, '3': None, '4': None, '5': None, '6': None, '7': None}
     while True:
         md5 = hashlib.md5()
         md5.update('%s%s' % (input_data, i))
         digest = md5.hexdigest()
         i += 1
         if digest[:5] == '00000':
-            code += digest[6]
-            print digest
-        if len(code) == 9:
+            print((digest, code))
+            if digest[5] in code.keys():
+                if code[digest[5]] is None:
+                    code[digest[5]] = digest[6]
+        if None not in code.values():
             break
-    print code
+    print(''.join([x[1] for x in sorted(code.items(), key=lambda x: x[0], reverse=False)]))
 
 if __name__ == '__main__':
     main()
