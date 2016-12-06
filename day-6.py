6d1f5b1735470c13d97f48228e6ef0ffe83c2977
Ernest W. Durbin III <ewdurbin@gmail.com>
2016-12-06 14:32:55 -0500

    day six part two

--- day-6.py
+++ day-6.py
@@ -7,7 +7,8 @@ def main():
     input_data = sys.stdin.read().rstrip().split('\n')
     decoded = ''
     for i in range(len(input_data[0])):
-        decoded += collections.Counter([x[i] for x in input_data]).most_common(1)[0][0]
+        frequencies = collections.Counter([x[i] for x in input_data])
+        decoded += sorted(frequencies.items(), key=lambda x: x[1])[0][0]
     print(decoded)
             
 if __name__ == '__main__':
