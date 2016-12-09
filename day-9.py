8eb766fafda07b3e29b4f7f446cfd2c1a7226b7f
Ernest W. Durbin III <ewdurbin@gmail.com>
2016-12-09 00:23:07 -0500

    day nine part two

--- day-9.py
+++ day-9.py
@@ -2,27 +2,31 @@
 
 import sys
 
-def main():
-    input_data = sys.stdin.read().rstrip()
-    decompressed = ''
+def decompress(input_str):
     sentinel = None
-    for i, character in enumerate(input_data):
+    decompressed = ''
+    for i, character in enumerate(input_str):
         if sentinel is not None and i <= sentinel:
             continue
         else:
             sentinel = None
         if character == '(':
             while True:
-                for j, character in enumerate(input_data[i:]):
+                for j, character in enumerate(input_str[i:]):
                     if character == ')':
                         break
                 break
-            length, repeats = input_data[i+1:i+j].split('x')
+            length, repeats = input_str[i+1:i+j].split('x')
             sentinel = i + j +  int(length)
-            decompressed += int(repeats) * input_data[i+j+1:i+j+1+int(length)]
+            decompressed += int(repeats) * decompress(input_str[i+j+1:i+j+1+int(length)])
         else:
             decompressed += character
-    print(len(decompressed))
+    return decompressed
+
+def main():
+    input_data = sys.stdin.read().rstrip()
+    decompressed = decompress(input_data)
+    print len(decompressed)
 
 if __name__ == '__main__':
     main()
