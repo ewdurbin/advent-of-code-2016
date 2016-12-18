3d1adfd7d6033072eae22475eac96c10bedbbc61
Ernest W. Durbin III <ewdurbin@gmail.com>
2016-12-18 00:51:11 -0500

    day eighteen part one

---
+++ day-18.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env/python
+
+import sys
+
+def main():
+    input_data = sys.stdin.read().rstrip()
+    board = [input_data]
+    for i in range(1,40):
+        previous_row = '.' + board[i-1] + '.'
+        row = ''
+        for i in range(len(previous_row)-2):
+            if previous_row[i:i+3] in ('^^.', '.^^', '^..', '..^'):
+                row += '^'
+            else:
+                row += '.'
+        board.append(row)
+    safe = 0
+    for row in board:
+        for spot in row:
+            if spot == '.':
+                safe += 1
+    print safe
+
+if __name__ == '__main__':
+    main()
+
