b6f8cdb66dcc4f312af2da168381043ea38b3f65
Ernest W. Durbin III <ewdurbin@gmail.com>
2016-12-19 01:28:24 -0500

    day nineteen part two: very slow... still runing

--- day-19.py
+++ day-19.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env/python
 
+from collections import deque
 import sys
 
 def main():
     input_data = int(sys.stdin.read().rstrip())
-    elves = range(input_data)
+    elves = deque(range(1,input_data+1))
     while len(elves) > 1:
-        if len(elves) % 2 == 0:
-            elves = elves[::2]
-        else:
-            elves = elves[2::2]
-    print elves[0] + 1
+        n = int(len(elves)/2)
+        elves.rotate(-n)
+        elves.popleft()
+        elves.rotate(n)
+        elves.rotate(-1)
+    print elves[0]
 
 if __name__ == '__main__':
     main()
