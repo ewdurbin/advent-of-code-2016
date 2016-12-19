57caeaf51c15bc0cc9aff99af71ada6c0adf9dbd
Ernest W. Durbin III <ewdurbin@gmail.com>
2016-12-19 18:01:47 -0500

    day nineteen part two, but fast

--- day-19.py
+++ day-19.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env/python
 
-from collections import deque
 import sys
 
 def main():
     input_data = int(sys.stdin.read().rstrip())
-    elves = deque(range(1,input_data+1))
-    while len(elves) > 1:
-        n = int(len(elves)/2)
-        elves.rotate(-n)
-        elves.popleft()
-        elves.rotate(n)
-        elves.rotate(-1)
-    print elves[0]
+    # used slow solution to look for a pattern in first 1000 or so results
+    # - find largest multiple of 3 which is less than target
+    # - from here "solutions" increment by 1 up to the first power of three's steps
+    # - through to the next power of three, solutions increment by 2
+    i = 1
+    while i * 3 < input_data:
+        i = i * 3
+    if i == input_data:
+        print input_data
+    else:
+        modifier = max(input_data - (2 * i), 0)
+        print input_data - i + modifier
 
 if __name__ == '__main__':
     main()
