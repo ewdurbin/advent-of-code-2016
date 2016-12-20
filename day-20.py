ae216e12bbf573d1d263bc680b9340c3c8b06ca6
Ernest W. Durbin III <ewdurbin@gmail.com>
2016-12-20 10:57:47 -0500

    day twenty part one

---
+++ day-20.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env/python
+
+import sys
+
+def main():
+    input_data = sys.stdin.read().rstrip().split('\n')
+    ranges = []
+    for interval in sorted(input_data, key=lambda x: int(x.split('-')[0])):
+        ranges.append(tuple((int(x) for x in interval.split('-'))))
+    ip, index = 0, 0
+    while ip <= 4294967295:
+        if ip >= ranges[index][0]:
+            if ip <= ranges[index][1]:
+                ip = ranges[index][1] + 1
+                continue
+            index += 1
+        else:
+            print ip
+            break
+
+if __name__ == '__main__':
+    main()
